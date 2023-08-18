7697cd0b0cf41c1c9de28119c8d7a62243d0241c
charz07 <charlottezhou6@gmail.com>
2023-08-18 13:56:50 -0700

    fixed myRigidBody error and streamlined cactus box collider logic

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -37,6 +37,7 @@ public class Movement : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("gameover");
         logic.gameOver();
         catAlive = false;
     }
